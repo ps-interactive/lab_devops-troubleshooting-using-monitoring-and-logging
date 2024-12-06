fcf4c05c77bf2e2e286c4adc19c0603fba48eaf0
Antonio Piedra <antoniojesuspiedra@gmail.com>
2024-12-06 12:29:55 +0100

    add setup file

---
+++ challenges/04/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Wait for Jenkins to fully start
+sleep 60
+
+# Obtain the initial admin password
+INITIAL_ADMIN_PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+# Download the Jenkins CLI jar
+http_proxy="$HTTP_PROXY" https_proxy="$HTTP_PROXY" curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
+
+# Install necessary plugins
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD install-plugin git workflow-aggregator
+
+# Create a Groovy script to create the admin user
+cat << EOF > create_admin_user.groovy
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('pslearner', 'pslearner')
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+EOF
+
+# Run the Groovy script to create the admin user
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < create_admin_user.groovy
+
+# Clean up
+# rm create_admin_user.groovy
+# rm jenkins-cli.jar
+
+echo "Jenkins setup complete. You can log in with username 'pslearner' and password 'pslearner'."
