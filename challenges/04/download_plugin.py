a661d864aede203402bd6f91b65508d9ebb90844
Antonio Piedra <antoniojesuspiedra@gmail.com>
2024-12-06 23:22:11 +0100

    Update download_plugin.py

--- challenges/04/download_plugin.py
+++ challenges/04/download_plugin.py
@@ -5,12 +5,12 @@ import requests
 
 
 def download_plugin(name,version):
-    pluginUrl = "http://updates.jenkins-ci.org/download/plugins/%s/%s/%s.hpi" % (name,version,name)
-    print "downloading: %s" % pluginUrl
+    url = "http://updates.jenkins-ci.org/download/plugins/%s/%s/%s.hpi" % (name,version,name)
+    print(f"downloading: {pluginUrl}")
     filename = "%s.hpi" % name
-	response = requests.get(url, proxies=proxies)
-	with open(filename, 'wb') as my_file:
-		my_file.write(response.content)
+    response = requests.get(url, proxies=proxies)
+    with open(filename, 'wb') as my_file:
+        my_file.write(response.content)
     download_dependencies(filename)
 
 def download_dependencies(filename):
@@ -21,7 +21,7 @@ def download_dependencies(filename):
     for dep in dependencies:
         _dep = dep.strip()
         _deps = _dep.split(":")
-        print _deps
+        print(_deps)
         name = _deps[1].strip()
         version = _deps[2].strip()
         download_plugin(name,version)
@@ -29,9 +29,9 @@ def download_dependencies(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print "usage sample: python download_jenkins_plugins.py junit 1.19"
+        print("usage sample: python download_plugin.py junit 1.19")
         sys.exit(1)
     name = sys.argv[1]
     version = sys.argv[2]
-    print "download %s %s" % (name,version)
+    print(f"download {name} {version}")
     download_plugin(name,version)
