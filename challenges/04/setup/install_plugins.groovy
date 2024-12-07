0dcb80bfef47cc07aa17fbd84eda3cb7e15daf04
Antonio Piedra <antoniojesuspiedra@gmail.com>
2024-12-07 20:15:59 +0100

    update jenkins setup

--- challenges/04/setup/install_plugins.groovy
+++ challenges/04/setup/install_plugins.groovy
@@ -1,37 +1,57 @@
 import jenkins.model.*
-import hudson.PluginManager
-import hudson.model.UpdateCenter
-import java.util.concurrent.Future
-
+import java.util.logging.Logger
+def logger = Logger.getLogger("install_plugins.groovy")
+def installed = false
+def initialized = false
+def fileSeperator=File.separator;
+println("Plugins Count Before Installation: "+Jenkins.instance.pluginManager.plugins.size())
+def filepath="/home/pslearner/challenges/04/setup"+fileSeperator+"pluginslist.cfg";
+println "pluginslist.cfg FilePath : $filepath"
+def pluginslist = []
 def instance = Jenkins.getInstance()
-def pm = instance.pluginManager
-def uc = instance.updateCenter
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
 
-// List of recommended plugins
-def plugins = [
-    'git',                 // Git plugin
-    'workflow-aggregator', // Pipeline
-    'blueocean',           // Blue Ocean
-    'credentials',         // Credentials
-    'job-dsl',             // Job DSL
-    'matrix-auth',         // Matrix Authorization Strategy
-    'cloudbees-folder',    // Folders
-	'prometheus',          // Prometheus metrics plugin
-]
-
-plugins.each {
-    if (!pm.getPlugin(it)) {
-        def plugin = uc.getPlugin(it)
-        if (plugin) {
-            Future installFuture = plugin.deploy(true)
-            installFuture.get()
-            println "Installed: ${it}"
-        } else {
-            println "Plugin not found: ${it}"
-        }
-    } else {
-        println "Already installed: ${it}"
+if(new File( filepath).exists()){
+    new File( filepath ).eachLine { line ->
+        pluginslist << line
     }
+	logger.info("Pluginslist count: "+pluginslist.size())
+	logger.info("Pluginslist to be installed: "+pluginslist)
+	
+	if(pluginslist.size()>0){
+	pluginslist.each {
+	  logger.info "-------------->>>>> Installing Plugin "+it.trim();
+	  if (!pm.getPlugin(it)) {
+		    logger.info("Looking UpdateCenter for " + it)
+		    if (!initialized) {
+		      uc.updateAllSites()
+		      initialized = true
+		    }
+		    def plugin = uc.getPlugin(it)
+		    if (plugin) {
+		      logger.info("Installing " + it)
+		        def installFuture = plugin.deploy()
+		      while(!installFuture.isDone()) {
+		        logger.info("Waiting for plugin install: " + it)
+		        sleep(3000)
+		      }
+		      installed = true
+		    }
+		  }
+		}
+		println("Plugins Count after Installation: "+Jenkins.instance.pluginManager.plugins.size())
+	}
+	else{
+		println("Zero Plugins specified in pluginslist.cfg file, skipping installation step")
+	}
+}
+else{
+	println("File Not Present @ Path: "+filepath)
 }
 
-instance.save()
+if (installed) {
+  logger.info("Plugins installed, initiating jenkins restart...!")
+  instance.save()
+  instance.restart()
+}
--- challenges/04/setup/setup.sh
+++ challenges/04/setup/setup.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-# Update JAVA_ARGS
+echo "Updating JAVA_ARGS"
 sed -i 's/-Djava.awt.headless=true/-Djenkins.install.runSetupWizard=false -Dhttp.auth.preference=basic -Djdk.http.auth.tunnelin.disabledSchemes= -Djava.awt.headless=true/' /lib/systemd/system/jenkins.service
 
-# Reload to apply changes
+echo "Reloading Jenkins to apply changes"
 systemctl daemon-reload
 systemctl restart jenkins
 
-# Obtain the initial admin password
+echo "Obtaining the initial admin password"
 INITIAL_ADMIN_PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
-# Download the Jenkins CLI jar
+echo "Download the Jenkins CLI jar"
 curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 
-# Extract proxy details from https_proxy env var
+echo "Extract proxy details from https_proxy env var"
 proxyAddress=$(echo $https_proxy | sed -r 's|https?://([^:]+):([^@]+)@([^:]+):([0-9]+)|\3|')
 proxyPort=$(echo $https_proxy | sed -r 's|https?://([^:]+):([^@]+)@([^:]+):([0-9]+)|\4|')
 proxyUser=$(echo $https_proxy | sed -r 's|https?://([^:]+):([^@]+)@([^:]+):([0-9]+)|\1|')
 proxyPassword=$(echo $https_proxy | sed -r 's|https?://([^:]+):([^@]+)@([^:]+):([0-9]+)|\2|')
 noProxy='localhost|127.0.0.1|::1'
 
-# Replace placeholders in Groovy script
+echo "Replace placeholders in Groovy script"
 sed -i "s/'proxyAddress'/\"$proxyAddress\"/" /home/pslearner/challenges/04/setup/setup_proxy.groovy
 sed -i "s/proxyPort/$proxyPort/" /home/pslearner/challenges/04/setup/setup_proxy.groovy
 sed -i "s/'proxyUser'/\"$proxyUser\"/" /home/pslearner/challenges/04/setup/setup_proxy.groovy
 sed -i "s/'proxyPassword'/\"$proxyPassword\"/" /home/pslearner/challenges/04/setup/setup_proxy.groovy
 sed -i "s/'noProxy'/\"$noProxy\"/" /home/pslearner/challenges/04/setup/setup_proxy.groovy
 
-# Setup proxy
+echo "Setup proxy"
 java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < /home/pslearner/challenges/04/setup/setup_proxy.groovy
 
-# Disable wizard
+echo "Disable wizard"
 java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < /home/pslearner/challenges/04/setup/disable_wizard.groovy
 
-# Install plugins
-# java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < /home/pslearner/challenges/04/setup/install_plugins.groovy
-https_proxy="$https_proxy" java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD install-plugin https://updates.jenkins.io/download/plugins/prometheus/795.v995762102f28/prometheus.hpi
+echo "Install plugins"
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < /home/pslearner/challenges/04/setup/install_plugins.groovy
+# https_proxy="$https_proxy" java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD install-plugin https://updates.jenkins.io/download/plugins/prometheus/795.v995762102f28/prometheus.hpi
 
-
-# Run the Groovy script to create the admin user
+echo "Create admin user"
 java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$INITIAL_ADMIN_PASSWORD groovy = < /home/pslearner/challenges/04/setup/create_admin_user.groovy
 
 # Clean up
