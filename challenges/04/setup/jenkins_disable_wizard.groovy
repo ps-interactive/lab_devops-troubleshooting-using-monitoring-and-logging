5078125e635746b86164d28d8f5867564dc0247c
elaina-nguyen <elaina-nguyen@pluralsight.com>
2026-04-20 12:03:07 -0500

    Refactor Jenkins disable wizard script
    Updated Jenkins script to use new method for instance retrieval and removed commented-out code.

--- challenges/04/setup/jenkins_disable_wizard.groovy
+++ challenges/04/setup/jenkins_disable_wizard.groovy
@@ -1,9 +1,20 @@
 #!groovy
 
-import jenkins.model.*
-import hudson.util.*;
-import jenkins.install.*;
+import jenkins.model.Jenkins
+import jenkins.install.InstallState
 
-def instance = Jenkins.getInstance()
+def instance = Jenkins.get()
 
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+instance.save()
+
+
+// #!groovy
+
+// import jenkins.model.*
+// import hudson.util.*;
+// import jenkins.install.*;
+
+// def instance = Jenkins.getInstance()
+
+// instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
