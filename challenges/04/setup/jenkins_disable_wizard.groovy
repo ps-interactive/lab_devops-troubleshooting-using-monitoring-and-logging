d303c161aaa4ed1f769939a545c1f85eb84113f7
elaina-nguyen <elaina-nguyen@pluralsight.com>
2026-04-23 10:02:20 -0500

    Uncomment Jenkins setup code in groovy script
    moving back to author's old script

--- challenges/04/setup/jenkins_disable_wizard.groovy
+++ challenges/04/setup/jenkins_disable_wizard.groovy
@@ -1,20 +1,22 @@
-// #!groovy
+//author's original
+ #!groovy
 
-// import jenkins.model.Jenkins
-// import jenkins.install.InstallState
+import jenkins.model.Jenkins
+import jenkins.install.InstallState
 
-// def instance = Jenkins.get()
-
-// instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
-// instance.save()
-
-
-// #!groovy
-
-import jenkins.model.*
-import hudson.util.*;
-import jenkins.install.*;
-
-def instance = Jenkins.getInstance()
+def instance = Jenkins.get()
 
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+instance.save()
+
+
+//docker setup
+// #!groovy
+
+// import jenkins.model.*
+// import hudson.util.*;
+// import jenkins.install.*;
+
+// def instance = Jenkins.getInstance()
+
+// instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
