bbf968bd6167d17eff409aef294877137b83804c
elaina-nguyen <elaina-nguyen@pluralsight.com>
2026-04-23 11:48:45 -0500

    Revert "Update jenkins_install_plugins.groovy"
    This reverts commit 32180655d8288c51634e8fff60d5b5e197566719.

--- challenges/04/setup/jenkins_install_plugins.groovy
+++ challenges/04/setup/jenkins_install_plugins.groovy
@@ -1,64 +1,63 @@
-// Docker's edit
-// import jenkins.model.*
-// def installed = false
-// def initialized = false
-
-// // Added 'workflow-aggregator' to enable the Pipeline feature
-// def pluginslist = ['prometheus', 'blueocean', 'workflow-aggregator']
-// def instance = Jenkins.getInstance()
-// def pm = instance.getPluginManager()
-// def uc = instance.getUpdateCenter()
-
-// pluginslist.each {
-//   println("-------------->>>>> Attempting to install Plugin "+it.trim());
-//   if (!pm.getPlugin(it)) {
-//     println("Looking UpdateCenter for " + it);
-//     if (!initialized) {
-//       uc.updateAllSites()
-//       initialized = true
-//     }
-//     def plugin = uc.getPlugin(it)
-//     if (plugin) {
-//       println("Installing " + it)
-//       def installFuture = plugin.deploy()
-//       while(!installFuture.isDone()) {
-//         println("Waiting for plugin install: " + it);
-//         sleep(3000)
-//       }
-//       installed = true
-//     }
-//   }
-// }
-
-// Author's Original Edit
 import jenkins.model.*
 def installed = false
 def initialized = false
-def fileSeperator=File.separator;
 
-def pluginslist = ['prometheus', 'blueocean']
+// Added 'workflow-aggregator' to enable the Pipeline feature
+def pluginslist = ['prometheus', 'blueocean', 'workflow-aggregator']
 def instance = Jenkins.getInstance()
 def pm = instance.getPluginManager()
 def uc = instance.getUpdateCenter()
 
-
 pluginslist.each {
   println("-------------->>>>> Attempting to install Plugin "+it.trim());
   if (!pm.getPlugin(it)) {
-		println("Looking UpdateCenter for " + it);
-		if (!initialized) {
-		  uc.updateAllSites()
-		  initialized = true
-		}
-		def plugin = uc.getPlugin(it)
-		if (plugin) {
-		  println("Installing " + it)
-			def installFuture = plugin.deploy()
-		  while(!installFuture.isDone()) {
-			println("Waiting for plugin install: " + it);
-			sleep(3000)
-		  }
-		  installed = true
-		}
-	  }
-	}
+    println("Looking UpdateCenter for " + it);
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      println("Installing " + it)
+      def installFuture = plugin.deploy()
+      while(!installFuture.isDone()) {
+        println("Waiting for plugin install: " + it);
+        sleep(3000)
+      }
+      installed = true
+    }
+  }
+}
+
+// Author's Original Edit
+// import jenkins.model.*
+// def installed = false
+// def initialized = false
+// def fileSeperator=File.separator;
+
+// def pluginslist = ['prometheus', 'blueocean']
+// def instance = Jenkins.getInstance()
+// def pm = instance.getPluginManager()
+// def uc = instance.getUpdateCenter()
+
+
+// pluginslist.each {
+//   println("-------------->>>>> Attempting to install Plugin "+it.trim());
+//   if (!pm.getPlugin(it)) {
+// 		println("Looking UpdateCenter for " + it);
+// 		if (!initialized) {
+// 		  uc.updateAllSites()
+// 		  initialized = true
+// 		}
+// 		def plugin = uc.getPlugin(it)
+// 		if (plugin) {
+// 		  println("Installing " + it)
+// 			def installFuture = plugin.deploy()
+// 		  while(!installFuture.isDone()) {
+// 			println("Waiting for plugin install: " + it);
+// 			sleep(3000)
+// 		  }
+// 		  installed = true
+// 		}
+// 	  }
+// 	}
