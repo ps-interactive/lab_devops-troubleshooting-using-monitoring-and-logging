d5d3e4a90468fd92ab73fe31fd8d4f249760747e
elaina-nguyen <elaina-nguyen@pluralsight.com>
2026-04-23 11:48:52 -0500

    Revert "Update jenkins_setup_proxy.groovy"
    This reverts commit 679fa3467565031b0de12094b50f22fd2546a2c0.

--- challenges/04/setup/jenkins_setup_proxy.groovy
+++ challenges/04/setup/jenkins_setup_proxy.groovy
@@ -1,24 +1,23 @@
-import jenkins.model.*
+import jenkins.model.Jenkins
 import hudson.ProxyConfiguration
 
-def instance = Jenkins.getInstance()
+def j = Jenkins.instance
+def proxy = new ProxyConfiguration("proxyAddress", proxyPort as int, "proxyUser", "proxyPassword", "noProxy")
+j.proxy = proxy
+j.save()
 
-def proxy = new ProxyConfiguration(
-    'proxyAddress',
-    proxyPort,
-    'proxyUser', // Optional: Proxy username
-    'proxyPassword', // Optional: Proxy password
-    'noProxy' // Optional: No proxy hosts
-)
-
-instance.proxy = proxy
-instance.save()
-
-
-// import jenkins.model.Jenkins
+// import jenkins.model.*
 // import hudson.ProxyConfiguration
 
-// def j = Jenkins.instance
-// def proxy = new ProxyConfiguration("proxyAddress", proxyPort as int, "proxyUser", "proxyPassword", "noProxy")
-// j.proxy = proxy
-// j.save()
+// def instance = Jenkins.getInstance()
+
+// def proxy = new ProxyConfiguration(
+//     'proxyAddress',
+//     proxyPort,
+//     'proxyUser', // Optional: Proxy username
+//     'proxyPassword', // Optional: Proxy password
+//     'noProxy' // Optional: No proxy hosts
+// )
+
+// instance.proxy = proxy
+// instance.save()
