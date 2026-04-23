e688b464ba977410ac0474d718af33dde12ba6ee
elaina-nguyen <elaina-nguyen@pluralsight.com>
2026-04-23 12:27:30 -0500

    Add print statement for proxy configuration confirmation
    Added a print statement to confirm proxy configuration.

--- challenges/04/setup/jenkins_setup_proxy.groovy
+++ challenges/04/setup/jenkins_setup_proxy.groovy
@@ -5,6 +5,7 @@ def j = Jenkins.instance
 def proxy = new ProxyConfiguration("proxyAddress", proxyPort as int, "proxyUser", "proxyPassword", "noProxy")
 j.proxy = proxy
 j.save()
+println("Proxy configured")
 
 // import jenkins.model.*
 // import hudson.ProxyConfiguration
